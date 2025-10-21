2af1d3564fb9a6185e966de6afdeb620700cb1e7
kiTTyaGIT <katya.goryunova.08@gmail.com>
2025-10-21 23:03:46 +0300

    Добавление коннекта employee с бд

---
+++ entity/employee_entity.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, Text, Sequence
+
+from db import Base
+
+
+class Employee(Base):
+    __tablename__ = "employee"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name = Column(Text)
+    surname = Column(Text)
+    patronymic = Column(Text)
+    phone_number = Column(Text)
+    mail = Column(Text)
+
+    def __repr__(self):
+        return f"<Employee(id={self.id}, username='{self.name}')>"
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello World")
+from db import get_db
+from repository.employee_repository import UserRepository
+
+
+def main():
+    db = next(get_db())
+    user_repo = UserRepository(db)
+
+    try:
+        # Создание пользователя
+        new_user = user_repo.create_user({
+            "name": "Петя",
+            "surname": "Петев",
+            "mail": "petya@example.com",
+            "phone_number": "+79999999997",
+        })
+        print(f"Created user: {new_user}")
+
+    except Exception as e:
+        print(f"Error: {e}")
+        db.rollback()
+    finally:
+        db.close()
+
+
+if __name__ == "__main__":
+    main()
